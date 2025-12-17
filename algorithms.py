23b461f7dc9d3801b6989e46021707f1defea0aa
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-17 12:12:02 +0500

    ShellSort added

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numba import njit
 
+
 @njit
 def BubbleSort(arr: np.ndarray, /) -> np.ndarray:
 
@@ -40,4 +41,21 @@ def InsertionSort(arr: np.ndarray, /) -> np.ndarray:
             arr[j] = arr[j - 1]
             j -= 1
         arr[j] = min_value
+    return arr
+
+
+@njit
+def ShellSort(arr: np.ndarray, /) -> np.ndarray:
+
+    h, n = 1, arr.shape[0]
+    while h < n // 3:
+        h = (h * 3) + 1
+    while h > 0:
+        for j in range(h, n):
+            min_value, k = arr[j], j
+            while k >= h and arr[k - h] > min_value:
+                arr[k] = arr[k - h]
+                k -= h
+            arr[k] = min_value
+        h //= 3
     return arr
