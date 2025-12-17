baed265f0927df7212a00ceac317e273190ba376
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-17 13:51:41 +0500

    count_sort file updated again in linear_time package

--- linear_time/count_sort.py
+++ linear_time/count_sort.py
@@ -3,23 +3,19 @@ from numba import njit
 
 
 @njit
-def CountSort(arr: np.ndarray, border: int, /) -> np.ndarray:
+def CountSort(arr: np.ndarray, /) -> np.ndarray:
 
     n = arr.shape[0]
+    border = arr.max()
     num = np.zeros(shape=border + 1, dtype=np.int64)
     auxiliary = np.zeros(shape=n, dtype=np.int64)
-
     for i in range(n):
         num[arr[i]] += 1
-
     for j in range(1, border + 1):
         num[j] += num[j - 1]
-
     for k in range(n - 1, -1, -1):
         auxiliary[num[arr[k]] - 1] = arr[k]
         num[arr[k]] -= 1
-
     for l in range(n):
         arr[l] = auxiliary[l]
-
     return arr
